ea6cf54f8211a19c9984f912db47da74c8900b1c
--lmriii <mrwolf.lmriii@gmail.com>
2022-05-17 12:26:49 -0700

    adding more inputs for notes

--- src/components/Noter.tsx
+++ src/components/Noter.tsx
@@ -2,13 +2,22 @@ import React, { useState } from 'react';
 import { Badge } from 'react-bootstrap';
 
 
-
+interface rawInput{
+    title: string;
+    date?: any;
+    note: string;
+}
 
 
 
 //Note Recorder = Noter
 export default function Noter() {
-    const [rawInput, setRawInput] = useState('');
+    const [rawInput, setRawInput] = useState({
+        title: '',
+        date: '',
+        note: '',
+
+    });
 
     const inputStyle = {
         width: "350px",
@@ -36,7 +45,77 @@ export default function Noter() {
                     <div className='col text-center'>
                         <h1>
                             <Badge className='text-align-center text-info bg-dark'>
-                                Enter Notes and Preview
+                                Enter Note Title
+                            </Badge>
+                        </h1>
+                    </div>
+                </div>
+                
+                <div className='row mt-4'>
+                    <div className='col-md-6' >
+                        <h4>
+                            <Badge className='text-align-center text-danger bg-dark'>
+                                Raw Input
+                            </Badge>
+                        </h4>
+                        <div className='mark-input' style={{ width: "100%", height: "100%" }}>
+                            <input
+                                type="text"
+                                className='input'
+                                placeholder='Title'
+                                style={{ width: "100%", backgroundColor: "skyBlue", boxShadow: "5px 5px" }}
+                                onChange={e => { rawInput.title = setRawInput(e.target.value) }}
+                            ></input>
+                        </div>
+                    </div>
+                    
+                    <div className='col-md-6'>
+                        <h4>
+                            <Badge className='text-align-center text-info bg-dark'>
+                                Preview
+                            </Badge>
+                        </h4>
+                        <div style={{ whiteSpace: "pre-wrap", backgroundColor: "grey", boxShadow: "5px 5px", textAlign: "left", border: "solid", width: "100%" }} className='text-align-left'>
+                            {rawInput.title}
+                        </div>
+                    </div>
+
+                </div>
+                <div className='row mt-4'>
+                    <div className='col-md-6' >
+                        <h4>
+                            <Badge className='text-align-center text-danger bg-dark'>
+                                Date Taken
+                            </Badge>
+                        </h4>
+                        <div className='mark-input' style={{ width: "100%", height: "100%" }}>
+                            <input
+                                type="date"
+                                className='input'
+                                placeholder='Date note was taken'
+                                style={{ width: "100%", backgroundColor: "skyBlue", boxShadow: "5px 5px" }}
+                                onChange={e => { rawInput.date = setRawInput(e.target.value) }}
+                            ></input>
+                        </div>
+                    </div>
+                    
+                    <div className='col-md-6'>
+                        <h4>
+                            <Badge className='text-align-center text-info bg-dark'>
+                                Preview
+                            </Badge>
+                        </h4>
+                        <div style={{ whiteSpace: "pre-wrap", backgroundColor: "grey", boxShadow: "5px 5px", textAlign: "left", border: "solid", width: "100%" }} className='text-align-left'>
+                            {rawInput.date}
+                        </div>
+                    </div>
+
+                </div>
+                <div className='row mt-4'>
+                    <div className='col text-center'>
+                        <h1>
+                            <Badge className='text-align-center text-info bg-dark'>
+                                Enter Notes
                             </Badge>
                         </h1>
                     </div>
@@ -51,8 +130,9 @@ export default function Noter() {
                         <div className='mark-input' style={{ width: "100%", height: "100%" }}>
                             <textarea
                                 className='input'
+                                placeholder='Enter notes here'
                                 style={{ width: "100%", height: "100%", backgroundColor: "skyBlue", boxShadow: "5px 5px" }}
-                                onChange={e => { setRawInput(e.target.value) }}
+                                onChange={e => { rawInput.note = setRawInput(e.target.value) }}
                             ></textarea>
                         </div>
                     </div>
@@ -63,7 +143,7 @@ export default function Noter() {
                             </Badge>
                         </h4>
                         <div style={{ whiteSpace: "pre-wrap", backgroundColor: "grey", boxShadow: "5px 5px", textAlign: "left", border: "solid", width: "100%", height: "100%" }} className='text-align-left'>
-                            {rawInput}
+                            {rawInput.note}
                         </div>
                     </div>
 
